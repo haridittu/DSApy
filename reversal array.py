bf440fd7215cda580e84bf8f9df7d44d494014dc
haridittu <haridittu@gmail.com>
2020-10-19 15:12:17 +0530

    second method

--- reversal array.py
+++ reversal array.py
@@ -1,12 +1,29 @@
-def reverselist(A,start,end):
-	while start < end:
-		A[start], A[end] = A[end], A[start]
-		start += 1
-		end -= 1
+# def reverselist(A,start,end):
+# 	while start < end:
+# 		A[start], A[end] = A[end], A[start]
+# 		start += 1
+# 		end -= 1
 	
 
-A = [5,4,3,2,1]
-# print[A]
-print("reversal Array is")
-reverselist(A,0,4)
+# A = [5,4,3,2,1]
+# # print[A]
+# print("reversal Array is")
+# reverselist(A,0,4)
+# print(A)
+
+# Recursive python program to reverse an array
+
+# Function to reverse A[] from start to end
+def reverseList(A, start, end):
+	if start >= end:
+		return
+	A[start], A[end] = A[end], A[start]
+	reverseList(A, start+1, end-1)
+
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
 print(A)
+reverseList(A, 0, 5)
+print("Reversed list is")
+print(A)
+# This program is contributed by Pratik Chhajer
